b63a5c21e1e6044757124459a293dcd05415d0ad
Hong-sk <sooyun9600@naver.com>
2022-09-22 17:18:08 +0900

    test : 초기 api 통신 테스트 코드 작성
    서버로부터 데이터를 받아서 출력하는 화면 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
+import axios from "axios";
 import React from "react";
 import "./App.css";
 import GlobalStyles from "./styles/GlobalStyles";
+import { useState } from "react";
 
 const App = () => {
+  const url = process.env.REACT_APP_API_URL;
+  const [state, setState] = useState("ddd");
+
+  fetch(url)
+    .then((res) => res.text())
+    .then((data) => setState(data));
+
   return (
     <>
       <GlobalStyles />
-      <div>시작페이지입니다.</div>
+      <p>{state}</p>
     </>
   );
 };
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
   entry: {
@@ -43,6 +46,9 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
     }),
   ],
 };
