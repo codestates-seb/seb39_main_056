732b0b5909fe453cfb3fabc72fa3a53296667c1b
daunjeong <qkrtjrfo1@google.com>
2022-09-23 09:22:45 +0900

    NEM-179 / design : 텍스트 컴포넌트 구현
    - H1 텍스트 스타일 추가
    - H2 텍스트 스타일 추가
    - H3 텍스트 스타일 추가
    - H4 텍스트 스타일 추

---
+++ client/src/components/ui/atoms/texts/H4.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const H4 = styled.p`
+  font-size: 15px;
+  color: #594a4e;
+  text-align: center;
+  font-weight: 400;
+  display: inline-block;
+  padding: 5px 5px;
+`;
+
+export default H4;
