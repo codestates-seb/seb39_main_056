a9731c717a95e632714721fe6ad1cd2dc3e26a21
Hong-sk <sooyun9600@naver.com>
2022-09-23 14:28:25 +0900

    초기환경설정 cra로 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import axios from "axios";
-import React from "react";
-import "./App.css";
-import GlobalStyles from "./styles/GlobalStyles";
-import { useState } from "react";
+import axios from 'axios';
+import React from 'react';
+import './App.css';
+import GlobalStyles from './styles/GlobalStyles';
+import { useState } from 'react';
 
 const App = () => {
-  const url = process.env.REACT_APP_API_URL;
-  const [state, setState] = useState("ddd");
+	const url = process.env.REACT_APP_API_URL;
+	const [state, setState] = useState('ddd');
 
-  fetch(url)
-    .then((res) => res.text())
-    .then((data) => setState(data));
+	fetch(url)
+		.then(res => res.text())
+		.then(data => setState(data));
 
-  return (
-    <>
-      <GlobalStyles />
-      <p>{state}</p>
-    </>
-  );
+	return (
+		<>
+			<GlobalStyles />
+			<p>{state}</p>
+		</>
+	);
 };
 
 export default App;
