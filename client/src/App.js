aa4a8a9aa46809e62eaf09c45360d2fb7670a2c7
daunjeong <qkrtjrfo1@google.com>
2022-09-26 16:47:55 +0900

    NEM-190 / feat : App.js에서 페이지라우팅 설정
    - MainPage : path="/"
    - Mypage : path="/mypage/:id"
    - ProductDetail : path="/product/:id"

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import BodyContainer from './components/atom/BodyContainer';
 import MainPage from './components/pages/MainPage';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Mypage from './components/pages/Mypage';
+import ProductDetail from './components/pages/ProductDetail';
 
 function App() {
 	return (
-		<div className="App">
-			<BodyContainer>
-				<MainPage />
-			</BodyContainer>
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<BodyContainer>
+					<Routes>
+						<Route path="/" element={<MainPage />} />
+						<Route path="/mypage/:id" element={<Mypage />} />
+						<Route path="/product/:id" element={<ProductDetail />} />
+					</Routes>
+				</BodyContainer>
+			</div>
+		</BrowserRouter>
 	);
 }
 
