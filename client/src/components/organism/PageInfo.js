7676f8af3458bce83f36705c286f9e51ee5ac494
git-daun <94218285+git-daun@users.noreply.github.com>
2022-10-06 09:08:02 +0900

    Feature/nem 191 회원 정보 페이지 (#37)
    * NEM - 190 / design : 초기화면설정
    
    - 화면에 필요없는 import 삭제
    
    * NEM-190 / design : PageHeader 컴포넌트 및 하위 컴포넌트 작성
    
    - PageHeader : PageName과 PageInfo를 묶는 상단 헤더
    - PageName : 현재 페이지 이름
    - PageInfo : 현재 페이지 설명(필요한 경우만 보임)
    
    * NEM-190 / design : FoodCatrgoryItems 클릭스타일추가
    
    - FoodCategory : FoodCatrgoryItems의 상위 컴포넌트
    - FoodCatrgoryItems : 배열에 따라 만들어지는 메인메이지 음식 카테고리
    
    * NEM-190 / design : FoodCategoryBtn 컴포넌트 추가
    
    - FoodCategoryBtn 컴포넌트 추가
    
    * NEM-190 / design : FoodList 추가
    
    - FoodList : FoodCard의 상위 컴포넌트
    - FoodCard : 메인페이지에서 보이는 개별 상품 정보
    
    * NEM-190 / design : 컴포넌트 반응형 스타일 추가
    
    - FoodCard 컴포넌트에 flex-direction 추가
    - FoodCategorySort 컴포넌트에 flex-direction 추가
    - FoodCatrgoryItems 컴포넌트에 flex-direction 추가
    
    * NEM-190 / design : FoodCategorySort 스타일 정리
    
    - FoodCategorySort 버튼에서 옵션선택으로 바꿈
    
    * NEM-190 / design : Floating 배너 컴포넌트 추가
    
    - Floating 배너 컴포넌트 추가
    - 하단 fixed 스타일 추가
    
    * NEM-190 / design : PageInfo 컴포넌트 추가
    
    - 메인 페이지 PageName 하단에 PageInfo 배치
    
    * NEM-90 / design : FoodItem 컴포넌트 추가
    
    - FoodItem 컴포넌트 추가
    - FoodCatrgoryItems state에 따라서 스타일 변경 완성
    
    * NEM-190 / feat : App.js에서 페이지라우팅 설정
    
    - MainPage : path="/"
    - Mypage : path="/mypage/:id"
    - ProductDetail : path="/product/:id"
    
    * NEM-190/ feat : 개별상품 클릭하면 상세페이지로 이동
    
    - FoodCard에 링크 연결
    
    * NEM-190 / feat : 메인+회원정보페이지
    
    - MainPage 브랜치에 회원정보 페이지 작업함
    - 회원정보 페이지 라우팅 및 테이블 기본 디자인 완료
    
    * NEM-191 / feat : 회원정보페이지에서 메인화면으로 라우팅
    
    - Mypage에 Link 추가
    - 정보수정, 정보삭제, 메인으로 버튼에 alert함수 추가
    
    * NEM-191 / feat : Mypage 회원정보 데이터 get
    
    - 더미데이터로 Fixed 정보 넣어주기(userName, eamilAddress)
    - 인풋창에 있는 정보 변수에 담아 console.log에 찍어주기
    
    * NEM-191 / feat : 마이페이지 정보 수정 기능
    
    - 더미데이터로 마이페이지 정보 수정 기능 구현
    
    * NEM-191 / feat : 정보수정 기능 추가
    
    - '정보수정' 버튼과 '회원탈퇴'버튼에 따라 다른 fetch함수 실행
    
    * NEM-191 / design : 디자인 추가
    
    - table에 기본 스타일 추가
    
    Co-authored-by: daunjeong <qkrtjrfo1@google.com>

--- client/src/components/organism/PageInfo.js
+++ client/src/components/organism/PageInfo.js
@@ -3,16 +3,16 @@ import H2 from '../atom/H2';
 import styled from 'styled-components';
 
 const Container = styled.div`
-	text-align: center;
-	margin-bottom: 20px;
+  text-align: center;
+  margin: 20px;
 `;
 
 const PageInfo = () => {
-	return (
-		<Container>
-			<H2> 현재 페이지의 제품을 보여줍니다. </H2>
-		</Container>
-	);
+  return (
+    <Container>
+      <H2> 현재 페이지의 제품을 보여줍니다. </H2>
+    </Container>
+  );
 };
 
 export default PageInfo;
