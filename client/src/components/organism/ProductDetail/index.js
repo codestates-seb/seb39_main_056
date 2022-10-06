ca2c5d094dba4180bfe89b53b1511b76a9607bb5
NR0617 <nara20210617@gmail.com>
2022-10-06 18:18:18 +0900

    NEM-222 / bug : 서버 요청 함수의 오류 수정
    - 오류 수정

--- client/src/components/organism/ProductDetail/index.js
+++ client/src/components/organism/ProductDetail/index.js
@@ -4,23 +4,30 @@ import { useState } from 'react';
 
 export const ProductPage = ({ productId, productData }) => {
   const [quantity, setQuantity] = useState(0);
-  const token = localStorage.getItem('token');
-
+  const token = localStorage.getItem('JWT TOKEN');
   const url = `${process.env.REACT_APP_API_URL}/cart`;
   //장바구니에 담기 눌렀을때
   const BuyProduct = async () => {
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        productId,
-        purchaseQuantity: quantity,
-      }),
-    }).then(response => console.log(response.json()));
+    try {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          productId,
+          purchaseQuantity: quantity,
+        }),
+      }).then(response => {
+        if (!response.ok) throw new Error('No Response');
+        console.log(response.json());
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
   };
+
   return (
     <>
       <Styled.CategoryPageTitle>Home > Food Category</Styled.CategoryPageTitle>
