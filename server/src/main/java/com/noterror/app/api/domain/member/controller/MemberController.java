50f7defa1c37331278f2167c8f22cf7f0dd5f776
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-11-01 15:24:45 +0900

    refactor : Member domain refactoring(회원 수정 메서드 수정)

--- server/src/main/java/com/noterror/app/api/domain/member/controller/MemberController.java
+++ server/src/main/java/com/noterror/app/api/domain/member/controller/MemberController.java
@@ -55,8 +55,8 @@ public class MemberController {
     @PutMapping("/info")
     public ResponseEntity putMember(
             @RequestBody @Valid UpdateInfoDto updateInfoDto) {
-        Member currentMember = updateInfoDto.toEntityWithEmail(currentUserEmail());
-        Member updateMember = memberService.updateMember(currentMember);
+        Member updateInfo = updateInfoDto.toEntityWithEmail(currentUserEmail());
+        Member updateMember = memberService.updateMember(updateInfo);
         MemberResponseDto response = new MemberResponseDto(updateMember);
 
         return new ResponseEntity<>(
