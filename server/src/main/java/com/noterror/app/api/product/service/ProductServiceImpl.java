e006fc35a548eb8098384a3cbb7ec13f8d7c9e6e
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-04-01 19:38:44 +0900

    update main page 출력시 회원 404 에러 해결

--- server/src/main/java/com/noterror/app/api/product/service/ProductServiceImpl.java
+++ server/src/main/java/com/noterror/app/api/product/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.noterror.app.api.product.service;
 
+import com.noterror.app.api.member.repository.MemberRepository;
 import com.noterror.app.api.product.dto.QueryParamDto;
 import com.noterror.app.api.product.repository.ProductRepository;
 import com.noterror.app.api.entity.Product;
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
+    private final MemberRepository memberRepository;
     private final ProductRepository productRepository;
 
     private String currentPageVegetarianName;
@@ -41,11 +43,21 @@ public class ProductServiceImpl implements ProductService {
         return getAllByQueryParam(queryParamDto);
     }
 
+    /**
+     * 해당 메서드는 회원이 전체 제품 조회의 기능에 접근할 때 수행합니다.
+     * 하지만 현재 비회원임에도, SecurityContextHolder 에 알 수 없는 이메일이 찍혀서 회원으로 접근하고 있습니다.
+     * 당연히 db에 없는 이메일이기 때문에 null 발생으로 제품 출력에 오류가 발생하고 있습니다.
+     * 현재까지 정확한 원인을 알 수 없어서 우선 해당 메서드에서 메일이 db에 있는 데이터인지 한번 더 확인하고 있습니다.
+     */
     @Override
-    public Page<Product> findProductsWhenAuthenticated(QueryParamDto queryParamDto, Member member) {
+    public Page<Product> findProductsWhenAuthenticated(QueryParamDto queryParamDto, String email) {
+
         if (isNull(queryParamDto.getVegetarian())) {
-            queryParamDto.setVegetarian(member.getVegetarianType());
+            memberRepository.findByEmail(email).ifPresentOrElse(
+                    member -> {queryParamDto.setVegetarian(member.getVegetarianType());},
+                    () -> queryParamDto.setVegetarian("플렉시테리언"));
         }
+
         return getAllByQueryParam(queryParamDto);
     }
 
